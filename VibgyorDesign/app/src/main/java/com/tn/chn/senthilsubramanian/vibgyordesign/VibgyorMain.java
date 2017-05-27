933467e824185c8bb5150220d1e4e57e326a4501
SenthilSubramanian <skumardpm@live.com>
2017-05-27 15:23:31 +0530

    VIBGYOR Design
    Assignment Vibgyor

---
+++ VibgyorDesign/app/src/main/java/com/tn/chn/senthilsubramanian/vibgyordesign/VibgyorMain.java
@@ -0,0 +1,13 @@
+package com.tn.chn.senthilsubramanian.vibgyordesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VibgyorMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vibgyor_main);
+    }
+}
