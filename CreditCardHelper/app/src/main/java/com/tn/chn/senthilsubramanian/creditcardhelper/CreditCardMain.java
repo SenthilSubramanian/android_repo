0a6f04f3e5059866c3d1ac71804ff683e1c582fc
SenthilSubramanian <skumardpm@live.com>
2017-05-31 00:58:31 +0530

    Credit Card Page Design
    Assignment 3.2

---
+++ CreditCardHelper/app/src/main/java/com/tn/chn/senthilsubramanian/creditcardhelper/CreditCardMain.java
@@ -0,0 +1,13 @@
+package com.tn.chn.senthilsubramanian.creditcardhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreditCardMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit_card_main);
+    }
+}
